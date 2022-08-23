c08cf992707811be6da531f7f9fb37f01a5a59bd
Nadia Jahan Juthy <juthynadia@gmail.com>
2022-08-23 22:09:11 +0300

    task 9 soln added

--- loopTask_1/main.js
+++ loopTask_1/main.js
@@ -6,7 +6,7 @@
     }
 } */
 
-/* Make a program that prints all positive numbers that are smaller than 100 and even, in this order: 2 98 4 96 6 94 … Print result in one line. */
+/* 2. Make a program that prints all positive numbers that are smaller than 100 and even, in this order: 2 98 4 96 6 94 … Print result in one line. */
 
 /* let num;   
  let result2 = 0;
@@ -20,7 +20,7 @@
     result += " " + 100-(num+2) + " ";
  } */
  /* console.log(result); */
-/*  Make a program that asks repeatedly from the user the distance (km) and time (h) and calculates average speed. The program ends when user gives 0 for the distance. (After getting 0, the program does not ask anything from the user.) */
+/*3.  Make a program that asks repeatedly from the user the distance (km) and time (h) and calculates average speed. The program ends when user gives 0 for the distance. (After getting 0, the program does not ask anything from the user.) */
 
 /*  function speed() {
     let distance, time;
@@ -36,7 +36,7 @@
 } */
 /*  speed(); */
 
-/* Make a program that asks 20 numbers from user. After that the program prints out how many of those numbers where even. */
+/* 4.Make a program that asks 20 numbers from user. After that the program prints out how many of those numbers where even. */
 
 /* function evenNumber () {
     for (i = 0; i <= 20; i++) {
@@ -49,7 +49,7 @@
 } */
 /* evenNumber (); */
 
-/* Make a program that asks numbers from the user, until user gives 0 and then program ends. In the end program prints out average of the numbers. */
+/* 5. Make a program that asks numbers from the user, until user gives 0 and then program ends. In the end program prints out average of the numbers. */
 /* 
 let num2;
 let sum = 0;
@@ -61,7 +61,7 @@ while (num2 != 0) {
 } */
 /* console.log(sum/(count-1)); */
 
-/* Make a program that asks 25 numbers form the user. In the end program prints out average of the numbers. */
+/* 6. Make a program that asks 25 numbers form the user. In the end program prints out average of the numbers. */
 let randNum2;
 let sum2 = 0;
 let counter = 0;
@@ -79,7 +79,7 @@ console.log(`numbers entered ${i}, average is ${average}`); */
 } while (counter !== 25); 
 console.log(`numbers entered ${i}, average is ${average}`); */
 
-/* Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers. */
+/* 7. Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers. */
 
 /* let firstNum = Number(prompt("first number"));
 let num3;
@@ -96,18 +96,41 @@ average2 = (sum + firstNum + num3)/counter3;
 console.log(average2); */
 
 
-/* Make a program that asks first how many numbers user wants to give to the program. After that program asks those numbers. In the end program prints out the smallest number that user gave. */
+/*8.  Make a program that asks first how many numbers user wants to give to the program. After that program asks those numbers. In the end program prints out the smallest number that user gave. */
 
-function numbers() {
+/* function numbers() {
     let num4;
     let number5;
-    let counter = Number(prompt("how many numbers user wants to give to the program?"));
+    let counter = Number(prompt("how many numbers do you want to give to the program?"));
     let smallestNum = 0;
-    for (num4=0; num4 <= counter; num4++){
-        number5 = prompt("Next number");
-        if (num4 <= smallestNum){
-            console.log("the smallest num is " + num4);
+    for (num4=0; num4 < counter; num4++){
+        number5 = Number(prompt("Next number"));
+        if (number5 >= 0 && number5 == smallestNum ){
+            number5 = smallestNum;
+        }else if (num4 < smallestNum){
+            number5 = smallestNum;
         }
     }
-};
-numbers();
+    console.log(number5);
+}
+numbers(); */
+
+
+/* 9. Make a program that asks ten numbers and in the end prints out two biggest numbers. */
+
+let biggestNum = 0;
+let secondBiggestNum = 0;
+let number5;
+    for (let num4 = 0; num4 < 10; num4++){
+        number5 = Number(prompt("Enter number"));
+        if (number5 > biggestNum ){
+            secondBiggestNum  = biggestNum;
+            biggestNum = number5;
+        }else if (number5 > secondBiggestNum){
+            secondBiggestNum = number5;
+        }
+    }
+console.log(`largest num is ${biggestNum}, second largest num is ${secondBiggestNum}`);
+
+
+/* 10. Make a program that asks ten numbers. Program calculates and prints out sum and average, also prints out the smallest and biggest number. */
