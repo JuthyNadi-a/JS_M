73764a4d99c0743da3bc7ba48a623149c563c2a4
JuthyNadi-a <juthynadia@gmail.com>
2022-08-24 12:33:12 +0300

    gas calculator file updated

--- Gasoline Calculator/main.js
+++ Gasoline Calculator/main.js
@@ -1,6 +1,21 @@
-function getGas() {
+const getGas = () => {
     let price = Number(document.getElementById("price").value);
-    let amount = Number(document.getElementById("amount").value);
-    let gasAmount = (amount/price);
-    return console.log(`Gas can be bought ${gasAmount.toFixed(2)} litres`) ;
+    let amount = +(document.querySelector("#amount").value);
+
+    let answer = document.querySelector("#answer");
+    let gasAmount = Math.floor(amount/price);
+    /* return console.log(`Gas can be bought ${gasAmount} litres`) ; */
+
+    /* let text;
+
+    if (gasAmount>=10){
+        text= `You could get ${gasAmount} litres`
+    } else {
+        text = `You could escape`
+    } */
+
+    answer.textContent = gasAmount;
+    /* answer.textContent = text; */
+
+
 };
