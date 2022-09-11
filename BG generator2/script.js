2fde3e02d5396b5b05ea6473a81e168754986cb1
JuthyNadi-a <juthynadia@gmail.com>
2022-09-11 22:36:11 +0300

    arrow functionalities added

--- BG generator2/script.js
+++ BG generator2/script.js
@@ -1,33 +1,74 @@
 let color1 = document.querySelector('#color1');
 let color2 = document.querySelector('#color2');
 let colorBox = document.querySelector(".displayColor");
-let linearDirection = document.querySelector('input[name="toDirection"]');
-/* let arrow1 = document.querySelector('#arrow1');
+let arrow1 = document.querySelector('#arrow1');
 let arrow2 = document.querySelector('#arrow2');
 let arrow3 = document.querySelector('#arrow3');
 let arrow4 = document.querySelector('#arrow4');
 let arrow5 = document.querySelector('#arrow5');
 let arrow6 = document.querySelector('#arrow6');
 let arrow7 = document.querySelector('#arrow7');
-let arrow8 = document.querySelector('#arrow8'); */
+let arrow8 = document.querySelector('#arrow8');
 let cssCode = document.querySelector('#result');
 
 function colorUpdate(event) {
-  console.log(linearDirection.value)
-    colorBox.style.background  = event.target.value
+    colorBox.style.background  = event.target.value;
     colorBox.style.background =
-    `linear-gradient(to ${linearDirection.value}, ${color1.value}, ${color2.value})`;
+    `linear-gradient(to right, ${color1.value}, ${color2.value})`;
     cssCode.textContent = colorBox.style.background;
-}
+  }
+  /*  Arrows */
+  arrow1.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to right bottom, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow2.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to bottom, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
   
-document.querySelector('input[name="toDirection"]').oninput=colorUpdate;
+  arrow3.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to left bottom, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow4.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow5.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to left, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow6.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to left top, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow7.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to top, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  arrow8.addEventListener('click', function (event){
+    colorBox.style.background  = event.target.value;
+    colorBox.style.background =
+    `linear-gradient(to right top, ${color1.value}, ${color2.value})`;
+    cssCode.textContent = colorBox.style.background;
+  }) 
+  
+
 color1.addEventListener("input", colorUpdate)
 color2.addEventListener("input", colorUpdate)
-arrow1.addEventListener("click", colorUpdate)
-arrow2.addEventListener("click", colorUpdate)
-arrow3.addEventListener("click", colorUpdate)
-arrow4.addEventListener("click", colorUpdate)
-arrow5.addEventListener("click", colorUpdate)
-arrow6.addEventListener("click", colorUpdate)
-arrow7.addEventListener("click", colorUpdate)
-arrow8.addEventListener("click", colorUpdate)
+
