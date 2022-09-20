3dc031a380b05aa2bac56b0a740d6b095cc52387
JuthyNadi-a <juthynadia@gmail.com>
2022-09-20 14:53:11 +0300

    cicle clicked function added

--- speedGame/main.js
+++ speedGame/main.js
@@ -2,16 +2,17 @@ const div = document.querySelector('.container');
 const startBtn = document.querySelector('.start');
 const stopBtn = document.querySelector('.stop');
 const active = document.querySelector('#active');
+const circles = document.querySelectorAll(".circle");
+const result = document.querySelector("#display");
+
 const startGame = () => {
-console.log('click');
+console.log('game started');
 active.addEventListener('click', activeCircle);
 for (let circle of circles) {
   circle.addEventListener("click", activeCircle);
 }
 }
-
-const circles = document.querySelectorAll(".circle");
-const result = document.querySelector("#display");
+/* cicle clicked */
 const activeCircle = (e) => {
         console.log('circle clicked');
         display.innerHTML = `ID of circle ${e.target.innerHTML} is ${e.target.id}`
