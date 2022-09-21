aac3d7960331bd4c0e0b8c279a631fb3e8709c60
JuthyNadi-a <juthynadia@gmail.com>
2022-09-21 14:35:14 +0300

    random munber added, highlight random circle added

---
+++ speedGame/extra.js
@@ -0,0 +1,41 @@
+/* for (let circle of circles) {
+    circle.addEventListener("click", activeCircle);
+  } */
+
+
+  /* let rand = []
+let  randomCircle = circles[Math.floor(Math.random()*circles.length)];
+rand.push(randomCircle);
+for (let counter = 0; counter < circles.length; counter++) {
+  let newCircle = circles[Math.floor(Math.random()*circles.length)];
+  while (rand.lastIndexOf(newCircle) !== -1) {
+    newCircle = circles[Math.floor(Math.random()*circles.length)];
+  }
+  rand.push(newCircle);
+}
+console.log(rand); */
+/* for (let i = circles; i <= circles.length; i++) {
+  let rand = [];
+  console.log(circles);
+  let  randomCircle = circles[Math.floor(Math.random()*circles.length)];
+  if (rand.indexOf(randomCircle) == -1) {
+    rand.push(randomCircle)
+  } else {
+    i--;
+  }
+  console.log(rand);
+}
+}
+random(circles); */
+/*     console.log(circles);
+  let rand = [];
+  for (let i = 0; i <= circles.length; i++) {
+    let  randomCircle = circles[Math.floor(Math.random()*(circles.length))];
+    let newCircle = rand.indexOf(randomCircle);
+    if(newCircle === -1) {
+      rand.push(randomCircle)
+    }
+  }
+
+  console.log(rand);
+  return rand; */
--- speedGame/main.js
+++ speedGame/main.js
@@ -3,28 +3,56 @@ const startBtn = document.querySelector('.start');
 const stopBtn = document.querySelector('.stop');
 const active = document.querySelector('#active');
 const circles = document.querySelectorAll(".circle");
-const result = document.querySelector("#display");
+const score = document.querySelector("#score");
 
+let activeNum = 0;
+let timer;
+/* Randomize circle */
+const getRndInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
 const startGame = () => {
-console.log('game started');
+  console.log('start');
+  let nextActive = newCircle(activeNum);
 
-  /* for (let circle of circles) {
-    circle.addEventListener("click", activeCircle);
-  } */
+  circles[nextActive].classList.toggle('active');
+  circles[activeNum].classList.remove('active');
 
-  /* console.log(circles); */
-  circles.forEach((circle) => {
-    /* console.log(circle); */
-    circle.addEventListener("click", activeCircle)
-  })
+  activeNum = nextActive;
+
+  console.log(` current active number is ${activeNum}`);
+
+  timer = setTimeout(startGame, 1000);
+
+  function newCircle(activeNum) {
+    let nextActive = getRndInt(0,3);
+    if (nextActive != activeNum) {
+      return nextActive;
+    } else {
+      return newCircle(activeNum);
+    }
+  }
 }
-/* cicle clicked */
+/* circle clicked */
 const activeCircle = (e) => {
-    display.innerHTML = `Circle ${e.target.innerHTML} is clicked`
+  console.log(`Circle ${e.target.innerHTML} is clicked`);
 }
+/* Update score with click */
+let count = 0;
+const scoreCount = () => {
+  count++;
+  score.textContent = count;
+};
+
+/* call  function */
+circles.forEach((circle) => {
+  circle.addEventListener("click", activeCircle);
+  circle.addEventListener("click", scoreCount);
+})
 /* Stop game */ 
 const stopGame = () => {
-    console.log('game ended');
+  console.log('game ended');
+  clearTimeout(timer);
 }
 stopBtn.addEventListener('click', stopGame)
 startBtn.addEventListener('click', startGame);
