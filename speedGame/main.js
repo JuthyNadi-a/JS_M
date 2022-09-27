977f18a4c301f74f47d8f92bfddc7f9bd917d601
JuthyNadi-a <juthynadia@gmail.com>
2022-09-27 14:33:39 +0300

    fixed button display,disble mode added, modal conditions added

--- speedGame/main.js
+++ speedGame/main.js
@@ -4,21 +4,30 @@ const stopBtn = document.querySelector('.stop');
 const active = document.querySelector('#active');
 const circles = document.querySelectorAll(".circle");
 const score = document.querySelector("#score");
-const overlay = document.querySelector('#overlay');
+const overlay = document.querySelector('.overlay');
 const closeBtn = document.querySelector('#closeButton')
 
 let activeNum = 0;
 let timer;
 let pace = 1000;
 let rounds= 0;
-
-
+let initialMode = false;
+let startSound;
+let endSound;
 const startGame = () => {
-
+  startSound = new sound("assests/gameStart.wav");
+  startSound.play();
+  initialMode = true;
+  stopBtn.style.display = 'initial';
+  startBtn.style.display = 'none';
   if (rounds >= 3) {
+    startSound = new sound("assests/gameStart.wav");
+    startSound.pause();
+    endSound = new sound ("assests/gameEnd.mp3");
+    endSound.play();
     return stopGame();
   }
-  console.log('start');
+
   let nextActive = newCircle(activeNum);
   
   circles[nextActive].classList.toggle('active');
@@ -26,12 +35,12 @@ const startGame = () => {
   
   activeNum = nextActive;
   
-  console.log(` current active number is ${activeNum}`);
   /* set timer */
   timer = setTimeout(startGame, pace);
   pace = pace - 10;
 
   rounds++;
+
 /* check random number to be unique */
   function newCircle(activeNum) {
     let nextActive = getRndInt(0,3);
@@ -41,44 +50,85 @@ const startGame = () => {
       return newCircle(activeNum);
     }
   }
+  /* circle clicked */
+  const activeCircle = (circle,i) => {
+    circle.addEventListener("click", function () {
+      if (circle.click && initialMode == true) {
+        activeCircle(i);
+      }
+    })
+  }
 }
 /* Randomize circle */
 const getRndInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-/* circle clicked */
-const activeCircle = (i) => {
-  console.log(`Circle ${i.target.innerHTML} is clicked`);
-}
-/* Update score with click */
-let count = 0;
-const scoreCount = (i) => {
-  if (i !== activeNum){
-    stopGame()
-  } else {
-    count++;
-    rounds--;
-    score.textContent = count;
-  }
-};
-
+  /* Update score with click */
+  let count = 0;
+  const scoreCount = (i) => {
+    if (i !== activeNum){
+      stopGame();
+      startSound = new sound("assests/gameStart.wav");
+      startSound.pause();
+      endSound = new sound ("assests/gameEnd.mp3");
+      endSound.play();
+    } else {
+      count++;
+      rounds--;
+      score.textContent = count;
+      if (count <=1) {
+        modalScore.textContent = `You caught ${count} butterfly!!Try again!`;
+      } else if (count >= 2 && count <= 5){
+        modalScore.textContent = `You caught ${count} butterflies!!You can be faster! Try again!`;
+      } else if (count >= 6 && count <= 30){
+        modalScore.textContent = `You caught ${count} butterflies!! You did good! Try to be faster!`;
+      }
+      else if (count > 30){
+        modalScore.textContent = `You caught ${count} butterflies!! That is super fast! Amazing!!`;
+      }
+    }
+  };
 /* call  function */
 circles.forEach((circle, i) => {
-  circle.addEventListener("click", () => scoreCount(i));
-  circle.addEventListener("click", () => activeCircle(i));
-})
+  circle.addEventListener("click", function () {
+    if (circle.click && initialMode == true) {
+      scoreCount(i)
+    }
+      });
+  });
+/* Show modal */
+const showModal = () => {
+  overlay.classList.toggle('show');
+}
 /* Stop game */ 
 const stopGame = () => {
-  console.log('game ended');
-  overlay.style.display='flex';
+  /* startSound = new sound("assests/gameStart.wav");
+  startSound.pause(); */
+  endSound = new sound ("assests/gameEnd.mp3");
+  endSound.play();
+  showModal();
+  initialMode = false;
   clearTimeout(timer);
 }
-
+/* Reset game */
 const resetGame = () => {
   window.location.reload();
 };
-
-
+/* Game Sound */
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
 stopBtn.addEventListener('click', stopGame)
 startBtn.addEventListener('click', startGame);
 closeBtn.addEventListener('click', resetGame);
